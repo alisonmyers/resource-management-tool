6135693e639c9b1e6ab300ae9e331198ba34e4d8
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-04 17:31:04 -0700

    update siteUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://alisonmyers.gatsbyjs.io/",
     title: "Alisons Portfolio",
   },
   plugins: [],
