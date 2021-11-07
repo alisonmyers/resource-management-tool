347e8550bb55d4c6fb2a2160f42709b6717e0a06
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-07 01:15:01 -0800

    update icons as lists

--- src/components/goals.js
+++ src/components/goals.js
@@ -1,12 +1,30 @@
 import React from "react"
 import GoalData from "../data/MyGoals.json"
 
+import { GiGraduateCap } from "@react-icons/all-files/gi/GiGraduateCap";
+import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
+import { FaPencilAlt } from "@react-icons/all-files/fa/FaPencilAlt";
+
+const icons = {
+  a: GiGraduateCap,
+  b: FaBeer,
+  c: FaPencilAlt,
+}
+
+const PostIcon = ({iconName}) => {
+  const Icon = icons[iconName];
+  return <Icon />
+}
+
 const GoalsHtml = () => (
     GoalData.goals.map((data, i) => {
         return (
             <div key={i}>
                 <h2 className="blog-post">#{data.goal_num} {data.goal_title}</h2>
                 <p className="blog-post">{data.goal_description}</p>
+                <ul class="icons"> 
+                    {data.goal_artifacts.map((x, i) => <li><PostIcon iconName={x}/></li>)}
+                </ul>
             </div>
         )
     }
