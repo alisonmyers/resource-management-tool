881bb7b5beb9b88e60656de4f3397ef64d191a5e
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-19 12:52:46 -0800

    reset and remove pdfviewer

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,10 @@
 import * as React from 'react'
 import Layout from '../components/my-layout'
-import PDFViewer from '../components/pdf-viewer';
-import PDFJSBackend from '../backends/pdfjs';
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>About Me Section.</p>
-        <div class="pdf-container"> 
-        <PDFViewer backend={PDFJSBackend} src={"../../ETEC520_1_.pdf"}/>
-        </div>
     </Layout>
   )
 }
