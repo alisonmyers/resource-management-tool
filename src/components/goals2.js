d250b694520743ab01d6a18aced62517ab17ca78
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-19 17:53:36 -0800

    delete goals2

--- src/components/goals2.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import CourseData from "../data/MyCourses.json"
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-
