df38900781d688abc7dedfe6d2f8ee506dd41e2a
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-20 16:49:09 -0800

    popup works

--- src/components/project-cards.js
+++ src/components/project-cards.js
@@ -1,16 +1,43 @@
 import React, {useState} from "react";
 import ProjectData from '../data/MyProjects.json';
-import {Button} from "react-bootstrap";
-import { render } from "react-dom";
+import {Modal, Button} from "react-bootstrap";
+import AwesomeSlider from "react-awesome-slider";
 
-const ProjectCards = () => (
-    ProjectData.projects.map((data, i) => {
-        return(
-            <div className="col-md-12">
-                <Button>{data.title}</Button>
-            </div>
-            
-        )
-    })
-)
+const ProjectCards = () => {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+    return(
+        ProjectData.projects.map((data, i) => {
+            return(
+                <div className="col-md-12">
+                    <Button onClick={handleShow}>{data.title}</Button>
+
+                    <Modal show={show} onHide={handleClose} id={i}
+                        size="lg"
+                        aria-labelledby="contained-modal-title-vcentre"
+                        centered
+                        className="modal-inside"
+                        >
+                        <Modal.Header closeButton>
+                            <Modal.Title>{data.title}</Modal.Title>
+                        </Modal.Header>
+                        
+                        <Modal.Body>
+                            <AwesomeSlider>
+                                <div>{data.description}</div>
+                                <div>{data.connections}</div>
+                            </AwesomeSlider>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <p>Here is the footer</p>
+                        </Modal.Footer>
+                    </Modal>
+                </div>
+                
+            )
+    }))
+}
 export default ProjectCards
