0ad541b7a60c0a9ba1f6ad972fe4a2f3c244e8ae
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-29 23:24:08 -0800

    clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,13 +12,9 @@ import ColourSection from '../components/adding-colour'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <Layout pageTitle="This Portfolio">
+    <Layout pageTitle="this ePortfolio">
 
-      <div className="main-heading">
-        <h3>Hello, and welcome to my ePortfolio! </h3>
-      </div>
-
-      <div>
+      <section>
       <p>The purpose of my ePortfolio is to make connections between the courses I have taken throughout the MET program in order to ensure and demonstrate my understanding of the concepts that will be valuable for future endeavours. Throughout the development of the ePortfolio I expect to revisit concepts and course work in order to make connections between the work I have completed.</p> 
         
       <p>Another outcome is to demonstrate what I have learned about technology use and adoption, in part through the development of the ePortfolio itself. I would like this ePortfolio to be flexible, and useful beyond the MET program. To ensure this, I will build a dynamic ePortfolio in a way that is sustainable and can act as a professional portfolio of more technical work in the future. To this end, I hope to learn and apply modern web development techniques. </p>
@@ -26,36 +22,38 @@ const IndexPage = () => {
       <p>The audience that best aligns with my outcomes and purpose for the ePortfolio are: myself (making connections, ensuring deeper learning, developing an ePortfolio that is flexible and sustainable), and potential future employers.</p>
 
       <h3>Exploring the ePortfolio</h3>
-      <p> This portfolio is designed to showcase learning, reflection, and artifacts that have taken place throughout my MET journey. Each of my goals and artifacts are dynamically tagged with icons to make connections between them. You will see highlighted text which indicates the abilility to hover for more information. </p>
-      </div>
+      <p> This portfolio is designed to showcase learning, reflection, and artifacts that have taken place throughout my MET journey. Each of my goals and artifacts are dynamically tagged with icons to make connections between them. You will see highlighted text which indicates the abilility to hover for more information. Each artifact you will see below is clickable. You can select the image to expand for more detail, or where available you can select the external link icon to view the artifact in a new page.</p>
+      </section>
       
-      <div class="todo-card">
-      <h3>WIP</h3>
-        <p>You'll notice some incomplete sections so far, but here are my plans:</p>
+      <section>
+        <div class="todo-card">
+        <h3>WIP</h3>
+          <p>You'll notice some incomplete sections so far, but here are my plans:</p>
 
-        <ul>
-          <li>1. Add highlighters / more information icons (and reduce text, so the user has the option of what they want to learn more about</li>
-          <li>2. Make icons "clickable" - right now many icons are static, but they should actually provide more information when interacted with (see 1.)</li>
-          <li>3. Add a vertical navigation. I am experimenting with a single page layout, but it requires better navigation options</li>
-        </ul>
-      </div>
-    
+          <ul>
+            <li>1. Add highlighters / more information icons (and reduce text, so the user has the option of what they want to learn more about</li>
+            <li>2. Make icons "clickable" - right now many icons are static, but they should actually provide more information when interacted with (see 1.)</li>
+            <li>3. Add a vertical navigation. I am experimenting with a single page layout, but it requires better navigation options</li>
+          </ul>
+        </div>
+        </section>
+      
 
 
-      <div> 
+      <section> 
         <h1 className="main-heading">My Goals</h1>
         <div className="grid grid-cols-2 gap-3">
         <GoalsHtml></GoalsHtml>
         </div>
-      </div>
+      </section>
 
 
-      <div> 
+      <section> 
         <h1 className="main-heading">My MET Journey</h1> 
         <SketchSection></SketchSection>
         <OutlineSection></OutlineSection>
         <ColourSection></ColourSection>
-      </div>
+      </section>
 
     </Layout>
   )
