06df554680714105b50627160524f98636a703d1
alisonmyers <alison.myers@sauder.ubc.ca>
2021-12-12 15:31:48 -0500

    useeffect

--- src/components/scroll-top.js
+++ src/components/scroll-top.js
@@ -4,25 +4,29 @@ import {FaArrowCircleUp} from '@react-icons/all-files/fa/FaArrowCircleUp';
 
 const ScrollArrow = () =>{
 
-    if (typeof window !== `undefined`){
-        window.location.replace(res.data) // Window call
-      }
+    React.useEffect (() => {
+        if (typeof window !== `undefined`){
+            window.location.replace(res.data) // Window call
+          }
+    
+      const [showScroll, setShowScroll] = useState(false)
+    
+      const checkScrollTop = () => {
+        if (!showScroll && window.pageYOffset > 500){
+          setShowScroll(true)
+        } else if (showScroll && window.pageYOffset <= 500){
+          setShowScroll(false)
+        }
+      };
+    
+      const scrollTop = () =>{
+        window.scrollTo({top: 0, behavior: 'smooth'});
+      };
+    
+      window.addEventListener('scroll', checkScrollTop)
 
-  const [showScroll, setShowScroll] = useState(false)
 
-  const checkScrollTop = () => {
-    if (!showScroll && window.pageYOffset > 500){
-      setShowScroll(true)
-    } else if (showScroll && window.pageYOffset <= 500){
-      setShowScroll(false)
-    }
-  };
-
-  const scrollTop = () =>{
-    window.scrollTo({top: 0, behavior: 'smooth'});
-  };
-
-  window.addEventListener('scroll', checkScrollTop)
+    })
 
   return (
         <FaArrowCircleUp className="scrollTop" onClick={scrollTop} style={{height: 40, display: showScroll ? 'flex' : 'none'}}/>
