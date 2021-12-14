bdeab97a8c2e5b1c66a9be89e7ef1ba11928a6ec
alisonmyers <alison.myers@sauder.ubc.ca>
2021-12-14 15:26:44 -0500

    replace strings

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,15 @@ import { StaticImage } from "gatsby-plugin-image"
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <Layout pageTitle="This ePortfolio">
+    <Layout pageTitle="My Portfolio">
 
       <section id="home.welcome">
               
-        <p>Welcome! You may be a present, past, or future MET student ... a fellow data analyst ... a passer by? This website has been designed to reflect upon my experience and to showcase artifacts developed throughout the University of British Columbia's Masters of Educational Technology ('the MET') program. On this home page you will discover some of my design decisions, as well as more about me and my goals for this portfolio.Below, you can watch my Video Tour to understand an overview of the website. In the <a href="https://alisonmyers.gatsbyjs.io/sketchbook">Sketchbook</a> you will find a showcase of projects that I have developed throughout my coursework. In the <a href="https://alisonmyers.gatsbyjs.io/blog">Blog</a> you will find reflections on the building of this ePortfolio, as well as my future directions. Finally, any <a href="https://alisonmyers.gatsbyjs.io/feedback">feedback</a> is welcome.</p>
+        <p>Welcome! You may be a present, past, or future MET student ... a fellow data analyst ... a passer by? This website has been designed to reflect upon my experience and to showcase projects developed throughout the University of British Columbia's Masters of Educational Technology ('the MET') program. On this home page you will discover some of my design decisions, as well as more about me and my goals for this portfolio. Below, you can watch my Video Tour to understand an overview of the website. In the <a href="https://alisonmyers.gatsbyjs.io/sketchbook">Sketchbook</a> you will find a showcase of projects that I have developed throughout my coursework. In the <a href="https://alisonmyers.gatsbyjs.io/blog">Blog</a> you will find reflections on the building of this ePortfolio, as well as my future directions. Finally, any <a href="https://alisonmyers.gatsbyjs.io/feedback">feedback</a> is welcome.</p>
 
         <h2>Video Tour</h2>
         
-        <iframe className="centre-this" width="600" height="400" src="https://www.youtube.com/embed/PUcIGSYCIXQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe className="center-this" width="600" height="400" src="https://www.youtube.com/embed/PUcIGSYCIXQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
  
 
         </section>
@@ -26,12 +26,18 @@ const IndexPage = () => {
       <h2>My MET Beginnings</h2>
 
       <p>Why a Masters of Educational Technology? The MET program was an opportunity to engage with the education of educational technology, after spending the past five years in the field on the technology side. My current role is embedded within a technology support unit in a higher education faculty.</p>
-
-      <p>I began my career as a data analyst with an interest in designing informational dashboards, with specific interests in Learning Analytics. However, dashboard design can only come after the understanding and collection of data. I soon recognized the need to develop some programming and data skills. This allowed me to build better data pipelines, and make more sophisticated transformations in order to improve the dashboards. Once I had a better understanding of data, I then realized the benefits of understanding the systems that data came from. I realized the benefit of exploring how interaction with systems generated data - both to understand the data, but to also influence how tools are used in order for useful data to be created. While technology creates data, the decisions that lead to technology selection and how it is used ultimately influence the data that is generated and how that data can be used. Educational technology selection may come from individual (teachers), or institutional levels (administration), among other levels. At the institution level, I gained a perspective of culture and infrastructure that influence technology use and selection. At the individual level I was introduced to pedagogy, learning theories, and instructor comfort with technology. Through engagement with my classmates, many of whom are educated and experienced in education, I also gained valuable perspective of how individuals make technology decisions in their classrooms.</p>
       
+      <div class="grid grid-cols-2">
 
-      <StaticImage src="../images/about-career-2.png" alt="My Career Diagram" className="object-center" />
+        <div>
+          <p>I began my career as a data analyst with an interest in designing informational dashboards, with specific interests in Learning Analytics. However, dashboard design can only come after the understanding and collection of data. I soon recognized the need to develop some programming and data skills. This allowed me to build better data pipelines, and make more sophisticated transformations in order to improve the dashboards. Once I had a better understanding of data, I then realized the benefits of understanding the systems that data came from. I realized the benefit of exploring how interaction with systems generated data - both to understand the data, but to also influence how tools are used in order for useful data to be created. While technology creates data, the decisions that lead to technology selection and how it is used ultimately influence the data that is generated and how that data can be used. Educational technology selection may come from individual (teachers), or institutional levels (administration), among other levels. At the institution level, I gained a perspective of culture and infrastructure that influence technology use and selection. At the individual level I was introduced to pedagogy, learning theories, and instructor comfort with technology. Through engagement with my classmates, many of whom are educated and experienced in education, I also gained valuable perspective of how individuals make technology decisions in their classrooms.</p>
+        </div>
 
+          <div>
+          <StaticImage src="../images/about-career-2.png" alt="My Career Diagram"/>
+        </div>
+
+      </div>
       </section>
 
       <section id="home.goals"> 
@@ -52,12 +58,12 @@ const IndexPage = () => {
         <blockquote><ul class='arrow-list-style'>
 
         
-      <li>“Speaking the language” of educators is important to be able to communicate with instructors or other curriculum and content designers and experts. Their experiences and decisions toward teaching will influence design and technology decisions. Understanding the pedagogy and learning theories that teachers ascribe to provides important contextual information for the selection and use of technology. In turn, these decisions can affect data generated from learning activities, by tools, and how this data can be useful for making decisions in the future.  
+      <li>“Speaking the language” of educators is important to be able to communicate with educators, curriculum and content designers, and other institutional experts. Their experiences and decisions toward education and learning will influence design and technology decisions. In particular, understanding the pedagogy and learning theories that teachers ascribe to provides important contextual information for the selection and use of educational technologies. 
         </li>
 
         <li>The selection of educational technologies, at any scale, require a critical analysis of the user needs in their relevant context. In order to influence the selection of technologies it is important to demonstrate the ability to use and/or create frameworks for such decisions. The selection of technologies should also build upon the educational context, which can be influenced by learning theories.</li>
 
-        <li>Demonstrating the ability to develop content and to build technologies is part of my last two goals. The development of the ePortfolio itself will help to demonstrate these competencies. I would like this ePortfolio to be flexible, and useful beyond the MET program. To ensure this, I have built a dynamic ePortfolio in a way that is sustainable (for me) and can act as a professional portfolio of more technical work in the future.</li>
+        <li>Demonstrating the ability to develop content and to build technologies is part of my last two goals. The development of the portfolio itself will help to demonstrate these competencies. I would like this portfolio to be flexible, and useful beyond the MET program. To ensure this, I have built a dynamic portfolio in a way that is sustainable (for me) and can act as a professional portfolio of more technical work in the future.</li>
 
         </ul></blockquote>
 
--- src/pages/sketchbook.js
+++ src/pages/sketchbook.js
@@ -18,12 +18,12 @@ const ArtifactsPage = () => {
 
         <div>
           <p>
-          The metaphor that I have chosen is the process of creating a piece of art: from a sketchbook to the final art piece. The metaphor is appropriate for my MET journey as I haven’t felt like it has been a linear path, and I wanted a metaphor that could reflect the iteration and understanding that has occurred through different courses. Important concepts might have started as a doodle or sketch, but through different courses the ideas became more clear. I picked up various “techniques” (technologies, methods of learning) throughout the program that I am now more comfortable using that add appropriate detail and "colour" to my artifacts. 
+          The metaphor that I have chosen is the process of creating a piece of art: from a sketchbook to the final art piece. The metaphor is appropriate for my MET journey as I haven’t felt like it has been a linear path, and I wanted a metaphor that could reflect the iteration and understanding that has occurred through different courses. Important concepts might have started as a doodle or sketch, but through different courses the ideas became more clear. I picked up various “techniques” (technologies, methods of learning) throughout the program that I am now more comfortable using that add appropriate detail and "colour" to my projects. 
           </p>
 
           <h2>Exploring the Projects</h2>
 
-          <p>This portfolio is designed to showcase learning, reflection, and artifacts that have taken place throughout my MET journey. Each of my goals and artifacts are dynamically tagged with icons to make connections between them. [Note: you can hover on a goal icon for a reminder of that goal, but it may take a few seconds to load]. Each artifact image you will see is "clickable". You can select the image to expand for more detail, and where available you can select the external link icon to view the artifact in more detail [Note: the selection of the external link icon will open a new tab].</p>
+          <p>This portfolio is designed to showcase learning, reflection, and projects that have taken place throughout my MET journey. Each of my goals and projects are dynamically tagged with icons to make connections between them. [Note: you can hover on a goal icon for a reminder of that goal, but it may take a few seconds to load]. Each project image you will see is "clickable". You can select the image to expand for more detail, and where available you can select the external link icon to view the project in more detail [Note: the selection of the external link icon will open a new tab].</p>
         </div>
 
 
