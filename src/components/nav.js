25ffde9faa6e98aab2ff76cae1757a06ac58df7f
alisonmyers <alison.myers@sauder.ubc.ca>
2021-12-14 10:48:17 -0500

    add selection style

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,27 +1,36 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 
+
+
 const NavBar = () => {
+
+    const colorPrimary="blue"
+    const activeStyle = {
+        color: "#9e7e9d", 
+        fontWeight: "bold"
+    };
+
     return (
         <nav>
         <ul className="nav-links">
         <li className="nav-link-item">
-            <Link to="/" className="nav-link-text">
+            <Link to="/" activeStyle={activeStyle}  className="nav-link-text">
             <p class="hover:text-white">Home</p>
             </Link>
         </li>
         <li className="nav-link-item">
-            <Link to="/sketchbook" className="nav-link-text">
+            <Link to="/sketchbook" activeStyle={activeStyle} className="nav-link-text">
             <p class="hover:text-white">Sketchbook</p>
             </Link>
         </li>
         <li className="nav-link-item">
-            <Link to="/blog" className="nav-link-text">
+            <Link to="/blog" activeStyle={activeStyle} className="nav-link-text">
             <p class="hover:text-white">Blog</p>
             </Link>
         </li>
         <li className="nav-link-item">
-            <Link to="/feedback" className="nav-link-text">
+            <Link to="/feedback" activeStyle={activeStyle} className="nav-link-text">
             <p class="hover:text-white">Feedback</p>
             </Link>
         </li>
