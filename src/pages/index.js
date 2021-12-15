972ccf7c57294c5f3134bed0d7bc62cab8049779
alisonmyers <alison.myers@sauder.ubc.ca>
2021-12-14 23:06:28 -0500

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,21 +23,22 @@ const IndexPage = () => {
         </section>
       
       <section id="home.background">
-      <h2>My MET Beginnings</h2>
+      <h2>Me & the MET</h2>
 
       <p>Why a Masters of Educational Technology? The MET program was an opportunity to engage with the education of educational technology, after spending the past five years in the field on the technology side. My current role is embedded within a technology support unit in a higher education faculty.</p>
       
-      <div class="grid grid-cols-2">
-
-        <div>
-          <p>I began my career as a data analyst with an interest in designing informational dashboards, with specific interests in Learning Analytics. However, dashboard design can only come after the understanding and collection of data. I soon recognized the need to develop some programming and data skills. This allowed me to build better data pipelines, and make more sophisticated transformations in order to improve the dashboards. Once I had a better understanding of data, I then realized the benefits of understanding the systems that data came from. I realized the benefit of exploring how interaction with systems generated data - both to understand the data, but to also influence how tools are used in order for useful data to be created. While technology creates data, the decisions that lead to technology selection and how it is used ultimately influence the data that is generated and how that data can be used. Educational technology selection may come from individual (teachers), or institutional levels (administration), among other levels. At the institution level, I gained a perspective of culture and infrastructure that influence technology use and selection. At the individual level I was introduced to pedagogy, learning theories, and instructor comfort with technology. Through engagement with my classmates, many of whom are educated and experienced in education, I also gained valuable perspective of how individuals make technology decisions in their classrooms.</p>
-        </div>
-
-          <div>
+      <h3>About Me</h3>
+    
+          <div style={{ minWidth: 300, maxWidth: 750, float: "right"}}>
           <StaticImage src="../images/about-career-2.png" alt="My Career Diagram"/>
-        </div>
+          </div>
+        
+          <p>Context is important, so let me begin with some context about myself. I began my career as a data analyst with an interest in designing informational dashboards, with specific interests in Learning Analytics. However, dashboard design can only come after the understanding and collection of data. I soon recognized the need to develop some programming and data skills. This allowed me to build better data pipelines, and make more sophisticated transformations in order to improve the dashboards. Once I had a better understanding of data, I then realized the benefits of understanding the systems that data came from. I realized the benefit of exploring how interaction with systems generated data - both to understand the data, but to also influence how tools are used in order for useful data to be created. This brings me to my experience in the MET program...</p>
+  
 
-      </div>
+      
+      <h3>... & The MET</h3>
+        <p>While technology creates data, the decisions that lead to technology selection and how it is used ultimately influence the data that is generated and how that data can be used. Educational technology selection may come from individual (teachers), or institutional levels (administration), among other levels. At the institution level, I gained a perspective of culture and infrastructure which influence technology use and selection. At the individual level I was introduced to pedagogy, learning theories, and instructor comfort with technology. Through engagement with my classmates, many of whom are educated and experienced in education, I also gained valuable perspective of how individuals make technology decisions in their classrooms.</p>
       </section>
 
       <section id="home.goals"> 
@@ -48,19 +49,6 @@ const IndexPage = () => {
         <div className="goal-card-grid">        
         <GoalsHtml></GoalsHtml>
         </div>
-
-      </section>
-
-      <section id="home.goals.expanded">
-        <blockquote><ul class='arrow-list-style'>
-
-        <li>“Speaking the language” of education is important to be able to communicate with educators, curriculum and content designers, and other institutional experts. Their experiences and decisions toward education and learning will influence design and technology decisions. For example, knowing the pedagogy and learning theories that teachers ascribe to is important because they provides important contextual information for the selection and use of educational technologies. Originally, this goal was focused on learning theories specifically. However, through the development of this portfolio I realized the focus was too narrow.</li>
-
-        <li>The selection of educational technologies, at any scale, require a critical analysis of the user needs in their relevant context. In order to influence the selection of technologies it is important to demonstrate the ability to use and/or create frameworks for such decisions. The selection of technologies should also build upon the educational context, which can be influenced by learning theories.</li>
-
-        <li>Demonstrating the ability to develop content and to build technologies is part of my last two goals. The development of the portfolio itself will help to demonstrate these competencies. I would like this portfolio to be flexible, and useful beyond the MET program. To ensure this, I have built a dynamic portfolio in a way that is sustainable (for me) and can act as a professional portfolio of more technical work in the future.</li>
-
-        </ul></blockquote>
 
       </section>
 
