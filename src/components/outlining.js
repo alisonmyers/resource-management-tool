582be839b1bc7b73e847c3e4d0190e93c866054e
alisonmyers <alison.myers@sauder.ubc.ca>
2022-02-12 11:25:56 -0800

    remove outline content

--- src/components/outlining.js
+++ src/components/outlining.js
@@ -45,13 +45,8 @@ const OutlineSection = () => {
 
       <div className="bg-white py-8">
 
-        <h2>Outline and Detail</h2>
+        <h2>ETEC 511</h2>
 
-        <blockquote>
-        Once an idea has formed and been sketched / changed, it is time to outline the sketch in a more permanent way. 
-        </blockquote>
-
-        <p>Outlining is about beginning to understand what areas really interested me, and could start applying some of the concepts I was learning. I had a better understanding of my own style, and some practice under my belt creating sketches that I would want to turn into something more. These projects are ones that demonstrate increased awareness and understanding of concepts through writing and applications.</p>
       </div>
 
         <Projects projectData={projects}/>
