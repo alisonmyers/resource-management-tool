dfc1a9315776b5761f2f94a7ccd73edffa0b9e32
Alison Myers <alison.myers@sauder.ubc.ca>
2022-02-21 16:05:34 -0800

    remove background

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import * as React from 'react'
 import Layout from '../components/my-layout'
 
 import OutlineSection from '../components/outlining'
-import bookshelfOutline from "../images/bookshelf-outline.png"
 
 
 const IndexPage = () => {
@@ -11,7 +10,7 @@ const IndexPage = () => {
         <Layout pageTitle="Resources">
         <section> 
 
-        <div className="bg-fixed md:bg-fixed bg-contain ..." style={{ backgroundImage: `url(${bookshelfOutline})` }}>
+        <div className="bg-fixed md:bg-fixed bg-contain ...">
         <OutlineSection></OutlineSection>
         </div>
 
