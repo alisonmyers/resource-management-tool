974a74d7cdac82248dcea3ff4236b7cb6ccdc46d
Alison Myers <alison.myers@sauder.ubc.ca>
2022-03-01 17:21:16 -0800

    rm date

--- src/components/resources.js
+++ src/components/resources.js
@@ -13,7 +13,6 @@ const ResourceSection = () => {
                 id
                 title
                 course
-                date
                 resourcetypes
                 description
                 links
