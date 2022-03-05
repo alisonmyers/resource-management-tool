eb7b13a89a03e4844a1531518d1176046fd83376
Alison Myers <alison.myers@sauder.ubc.ca>
2022-03-05 15:08:21 -0800

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         path: `${__dirname}/src/data`,
       }
     },
+    'gatsby-source-google-spreadsheets',
     {
       resolve: 'gatsby-source-google-spreadsheets',
       options: {
