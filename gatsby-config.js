0acef73b66c74f36226cdfc6d496aa47aeb55a2c
Alison Myers <alison.myers@sauder.ubc.ca>
2022-03-26 12:04:21 -0700

    add mdx config

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,14 @@ module.exports = {
         name: `data`,
         path: `${__dirname}/src/data`,
       }
+    },
+    "gatsby-plugin-mdx",
+      {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects`,
+      }
     }
   ],
 };
