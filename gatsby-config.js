3072019050bbdd27e50c5d7f4dba206e58823456
Alison Myers <alison.myers@sauder.ubc.ca>
2022-03-26 12:10:06 -0700

    add posts summary components

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
       {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: `projects`,
-        path: `${__dirname}/src/projects`,
+        name: `summary`,
+        path: `${__dirname}/src/summary`,
       }
     }
   ],
---
+++ src/components/post-layout.js
@@ -0,0 +1,51 @@
+import * as React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import ScrollToTop from './scroll-top'
+
+const PostLayout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `)
+
+  return (
+    
+    <React.Fragment>
+
+      <div className ="global-header">   
+        <div className="global-header-div">    
+        <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      
+        <h2 className="site-title">{data.site.siteMetadata.title}</h2>
+        <h3 className="site-description">{data.site.siteMetadata.description}</h3>
+       
+        </div>
+      </div>
+      
+      <div>
+        <main className ="global-wrapper">
+          
+          <h1 className="main-heading">{pageTitle}</h1>
+
+          {children}
+        </main>
+
+      </div>
+
+      
+      {ScrollToTop()}
+      
+
+    </React.Fragment>
+
+    
+  )
+}
+
+export default PostLayout
