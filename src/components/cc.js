f988fbaaa70355eaaa67dc25d00dbc3b810f62a3
Alison Myers <alison.myers@sauder.ubc.ca>
2022-04-01 16:28:37 -0700

    add cc

---
+++ src/components/cc.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+export default function CC() {
+
+    return(
+
+        <div>
+        <a rel="license" 
+        href="http://creativecommons.org/licenses/by-nc/4.0/">
+        <img alt="Creative Commons License" 
+    
+        src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" />
+        </a>
+        </div>
+    )
+
+
+} 
